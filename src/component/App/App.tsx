7041d829ebba9ccba0e3a804ae1075f004a44742
Vitaly Kolesnik <vitaly.kolesnik62@gmail.com>
2022-12-01 13:41:45 +0300

    Init react app with tailwind

---
+++ src/component/App/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container text-center mx-auto bg-slate-500">
+      <h1 className="py-2">TS REACT REDUX TAILWIND</h1>
+    </div>
+  );
+}
+
+export default App;
